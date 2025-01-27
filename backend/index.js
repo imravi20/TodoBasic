322a70580bb14d9babc8486aa078ce9f515e7324
imravi20 <rabimajumder08@gmail.com>
2025-01-27 21:32:07 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const cors=require("cors");
 require("dotenv").config();
 const { zodSchemaA, zodSchemaB } = require("./zodVerify.js");
 const { todos } = require("./db.js");
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("ok");
